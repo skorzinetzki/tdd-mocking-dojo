c16ae9bd9a7d6a675732136eb4d148360d1dc977
Martin Dieblich <martin.dieblich@lise.de>
2016-11-16 18:45:05 +0100

    wrote first Test: CalculateFee for case "EUR"

--- lise.dojo.shop.tests/PriceCalculatorTest.cs
+++ lise.dojo.shop.tests/PriceCalculatorTest.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using lise.dojo.shop.currency;
 using NUnit.Framework;
 
 namespace lise.dojo.shop.tests
@@ -7,10 +8,15 @@ namespace lise.dojo.shop.tests
     public class PriceCalculatorTest
     {
         [Test]
-        public void PriceCalculatorIsAvailable()
+        public void PriceCalculator_CalculateFee_NoFeeForEuro()
         {
             var priceCalculator = new PriceCalculator();
-            priceCalculator.Should().BeOfType<PriceCalculator>();
+            double originalPrice = 100;
+
+            double expectedFee = 0;
+            double calculatedFee = priceCalculator.CalculateFee(originalPrice, Currency.EUR);
+
+            Assert.AreEqual(expectedFee, calculatedFee);
         }
     }
 }
--- lise.dojo.shop/PriceCalculator.cs
+++ lise.dojo.shop/PriceCalculator.cs
@@ -1,6 +1,13 @@
-﻿namespace lise.dojo.shop
+﻿using System;
+using lise.dojo.shop.currency;
+
+namespace lise.dojo.shop
 {
     public class PriceCalculator
     {
+        public double CalculateFee(double originalPrice, Currency cNY)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
