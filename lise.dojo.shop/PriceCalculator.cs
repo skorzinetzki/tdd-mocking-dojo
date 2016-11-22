42b1c55372082cc9f5d0a1e13ec110e29ed780d4
Martin Dieblich <martin.dieblich@lise.de>
2016-11-22 17:28:56 +0100

    [Refactor] Changed API of PriceCalculator to be more Factory-Like

--- lise.dojo.shop/PriceCalculator.cs
+++ lise.dojo.shop/PriceCalculator.cs
@@ -5,6 +5,45 @@ namespace lise.dojo.shop
 {
     public class PriceCalculator
     {
+
+        private readonly Currency _currencyToConvertTo;
+        private readonly double _rate;
+        private readonly double _minimumFee;
+
+        private PriceCalculator(Currency currencyToConvertTo, double rate, double minimumFee)
+        {
+            _currencyToConvertTo = currencyToConvertTo;
+            _rate = rate;
+            _minimumFee = minimumFee;
+        }
+
+        public static PriceCalculator GetPriceCalculator(Currency currencyToConvertTo)
+        {
+            switch(currencyToConvertTo)
+            {
+                case Currency.EUR:
+                    return new PriceCalculator(currencyToConvertTo, 0, 0);
+                case Currency.GBP:
+                    return new PriceCalculator(currencyToConvertTo, 0.05, 0);
+                case Currency.CHF:
+                    return new PriceCalculator(currencyToConvertTo, 0.03, 0);
+                case Currency.DKK:
+                    return new PriceCalculator(currencyToConvertTo, 0.04, 0);
+                case Currency.USD:
+                    return new PriceCalculator(currencyToConvertTo, 0.06, 8);
+                case Currency.CAD:
+                    return new PriceCalculator(currencyToConvertTo, 0.06, 9);
+                default:
+                    return new PriceCalculator(currencyToConvertTo, 0.07, 10);
+            }
+        }
+
+        [Obsolete("Use the factory-method instead")]
+        public PriceCalculator()
+        {
+        }
+
+        [Obsolete("Use the constructor with the factory-method GetPriceCalculator() instead, then call this function without a currency")]
         public double CalculateFee(double originalPrice, Currency currency)
         {
             if (originalPrice < 0)
@@ -17,6 +56,21 @@ namespace lise.dojo.shop
             if (fee < minimumFee)
             {
                 return minimumFee;
+            }
+            return fee;
+        }
+
+        public double CalculateFee(double originalPrice)
+        {
+            if (originalPrice < 0)
+            {
+                throw new InvalidPriceException("originalPrice", originalPrice, "Price must not be negative for calculating a fee(currency=" + _currencyToConvertTo + ").");
+            }
+            double fee = originalPrice * _rate;
+            
+            if (fee < _minimumFee)
+            {
+                return _minimumFee;
             }
             return fee;
         }
@@ -41,6 +95,7 @@ namespace lise.dojo.shop
             }
         }
 
+        [Obsolete("Use the constructor with the factory-method GetPriceCalculator() instead, then call this function without a currency")]
         public double GetMinimumFee(Currency currency)
         {
             switch (currency)
@@ -57,6 +112,10 @@ namespace lise.dojo.shop
                 default:
                     return 10;
             }
+        }
+        public double GetMinimumFee()
+        {
+            return _minimumFee;
         }
     }
 }
