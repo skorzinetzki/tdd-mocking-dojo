2db954768b90c7919127785c33cf014fa7337e9b
Martin Dieblich <martin.dieblich@lise.de>
2016-11-22 17:35:57 +0100

    [Refactor] Removed unused and obsolete code

--- lise.dojo.shop/PriceCalculator.cs
+++ lise.dojo.shop/PriceCalculator.cs
@@ -38,28 +38,6 @@ namespace lise.dojo.shop
             }
         }
 
-        [Obsolete("Use the factory-method instead")]
-        public PriceCalculator()
-        {
-        }
-
-        [Obsolete("Use the constructor with the factory-method GetPriceCalculator() instead, then call this function without a currency")]
-        public double CalculateFee(double originalPrice, Currency currency)
-        {
-            if (originalPrice < 0)
-            {
-                throw new InvalidPriceException("originalPrice", originalPrice, "Price must not be negative for calculating a fee(currency=" + currency + ").");
-            }
-            double fee = originalPrice * GetRate(currency);
-
-            double minimumFee = GetMinimumFee(currency);
-            if (fee < minimumFee)
-            {
-                return minimumFee;
-            }
-            return fee;
-        }
-
         public double CalculateFee(double originalPrice)
         {
             if (originalPrice < 0)
@@ -74,45 +52,7 @@ namespace lise.dojo.shop
             }
             return fee;
         }
-
-        private double GetRate(Currency currency)
-        {
-            switch (currency)
-            {
-                case Currency.EUR:
-                    return 0;
-                case Currency.GBP:
-                    return 0.05;
-                case Currency.CHF:
-                    return 0.03;
-                case Currency.DKK:
-                    return 0.04;
-                case Currency.USD:
-                case Currency.CAD:
-                    return 0.06;
-                default:
-                    return 0.07;
-            }
-        }
-
-        [Obsolete("Use the constructor with the factory-method GetPriceCalculator() instead, then call this function without a currency")]
-        public double GetMinimumFee(Currency currency)
-        {
-            switch (currency)
-            {
-                case Currency.EUR:
-                case Currency.GBP:
-                case Currency.CHF:
-                case Currency.DKK:
-                    return 0;
-                case Currency.USD:
-                    return 8;
-                case Currency.CAD:
-                    return 9;
-                default:
-                    return 10;
-            }
-        }
+        
         public double GetMinimumFee()
         {
             return _minimumFee;
