451e42371bcc9665e6070c55fb2ab51869c2fe8c
Martin Dieblich <martin.dieblich@lise.de>
2016-11-22 17:33:45 +0100

    [Refactor] Changed Tests to use the new API

--- lise.dojo.shop.tests/PriceCalculatorTest.cs
+++ lise.dojo.shop.tests/PriceCalculatorTest.cs
@@ -10,22 +10,22 @@ namespace lise.dojo.shop.tests
         [Test]
         public void PriceCalculator_CalculateFee_NoFeeForEuro()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.EUR);
             double originalPrice = 100;
 
             double expectedFee = 0;
-            double calculatedFee = priceCalculator.CalculateFee(originalPrice, Currency.EUR);
+            double calculatedFee = priceCalculator.CalculateFee(originalPrice);
 
             Assert.AreEqual(expectedFee, calculatedFee);
         }
         [Test]
         public void PriceCalculator_CalculateFee_Default_FeeAbove10Euro()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.CNY);
             double originalPrice = 1000;
 
             double expectedFee = originalPrice * 0.07;
-            double calculatedFee = priceCalculator.CalculateFee(originalPrice, Currency.CNY);
+            double calculatedFee = priceCalculator.CalculateFee(originalPrice);
 
             Assert.AreEqual(expectedFee, calculatedFee);
         }
@@ -33,11 +33,11 @@ namespace lise.dojo.shop.tests
         [Test]
         public void PriceCalculator_CalculateFee_Default_FeeBelow10Euro()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.CNY);
             double originalPrice = 100;
 
             double expectedFee = 10;
-            double calculatedFee = priceCalculator.CalculateFee(originalPrice, Currency.CNY);
+            double calculatedFee = priceCalculator.CalculateFee(originalPrice);
 
             Assert.AreEqual(expectedFee, calculatedFee);
         }
@@ -45,107 +45,107 @@ namespace lise.dojo.shop.tests
         [Test]
         public void PriceCalculator_CalculateFee_GBP()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.GBP); 
             double originalPrice = 1000;
 
             double expectedFee = originalPrice * 0.05;
-            double calculatedFee = priceCalculator.CalculateFee(originalPrice, Currency.GBP);
+            double calculatedFee = priceCalculator.CalculateFee(originalPrice);
 
             Assert.AreEqual(expectedFee, calculatedFee);
         }
         [Test]
         public void PriceCalculator_GetMinimumFee_GBP_HasNoMinimumFee()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.GBP);
 
             double noFee = 0;
-            double minimumFee = priceCalculator.GetMinimumFee(Currency.GBP);
+            double minimumFee = priceCalculator.GetMinimumFee();
 
             Assert.AreEqual(noFee, minimumFee);
         }
         [Test]
         public void PriceCalculator_CalculateFee_CHF()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.CHF);
             double originalPrice = 1000;
 
             double expectedFee = originalPrice * 0.03;
-            double calculatedFee = priceCalculator.CalculateFee(originalPrice, Currency.CHF);
+            double calculatedFee = priceCalculator.CalculateFee(originalPrice);
 
             Assert.AreEqual(expectedFee, calculatedFee);
         }
         [Test]
         public void PriceCalculator_GetMinimumFee_CHF_HasNoMinimumFee()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.CHF);
 
             double noFee = 0;
-            double minimumFee = priceCalculator.GetMinimumFee(Currency.CHF);
+            double minimumFee = priceCalculator.GetMinimumFee();
 
             Assert.AreEqual(noFee, minimumFee);
         }
         [Test]
         public void PriceCalculator_CalculateFee_DKK()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.DKK);
             double originalPrice = 1000;
 
             double expectedFee = originalPrice * 0.04;
-            double calculatedFee = priceCalculator.CalculateFee(originalPrice, Currency.DKK);
+            double calculatedFee = priceCalculator.CalculateFee(originalPrice);
 
             Assert.AreEqual(expectedFee, calculatedFee);
         }
         [Test]
         public void PriceCalculator_GetMinimumFee_DKK_HasNoMinimumFee()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.DKK);
 
             double noFee = 0;
-            double minimumFee = priceCalculator.GetMinimumFee(Currency.DKK);
+            double minimumFee = priceCalculator.GetMinimumFee();
 
             Assert.AreEqual(noFee, minimumFee);
         }
         [Test]
         public void PriceCalculator_CalculateFee_USD_FeeAbove8Euro()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.USD);
             double originalPrice = 1000;
 
             double expectedFee = originalPrice * 0.06;
-            double calculatedFee = priceCalculator.CalculateFee(originalPrice, Currency.USD);
+            double calculatedFee = priceCalculator.CalculateFee(originalPrice);
 
             Assert.AreEqual(expectedFee, calculatedFee);
         }
         [Test]
         public void PriceCalculator_CalculateFee_USD_FeeBelow8Euro()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.USD);
             double originalPrice = 100;
 
             double expectedFee = 8;
-            double calculatedFee = priceCalculator.CalculateFee(originalPrice, Currency.USD);
+            double calculatedFee = priceCalculator.CalculateFee(originalPrice);
 
             Assert.AreEqual(expectedFee, calculatedFee);
         }
         [Test]
         public void PriceCalculator_CalculateFee_CAD_FeeAbove9Euro()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.CAD);
             double originalPrice = 1000;
 
             double expectedFee = originalPrice * 0.06;
-            double calculatedFee = priceCalculator.CalculateFee(originalPrice, Currency.CAD);
+            double calculatedFee = priceCalculator.CalculateFee(originalPrice);
 
             Assert.AreEqual(expectedFee, calculatedFee);
         }
         [Test]
         public void PriceCalculator_CalculateFee_CAD_FeeBelow9Euro()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.CAD);
             double originalPrice = 100;
 
             double expectedFee = 9;
-            double calculatedFee = priceCalculator.CalculateFee(originalPrice, Currency.CAD);
+            double calculatedFee = priceCalculator.CalculateFee(originalPrice);
 
             Assert.AreEqual(expectedFee, calculatedFee);
         }
@@ -153,9 +153,9 @@ namespace lise.dojo.shop.tests
         [Test]
         public void PriceCalculator_CalculateFee_ThrowsExceptionOnNegativePrices()
         {
-            var priceCalculator = new PriceCalculator();
+            var priceCalculator = PriceCalculator.GetPriceCalculator(Currency.CAD);
             double negativePrice = -100;
-            Assert.Throws<InvalidPriceException>(() => priceCalculator.CalculateFee(negativePrice, Currency.CAD));
+            Assert.Throws<InvalidPriceException>(() => priceCalculator.CalculateFee(negativePrice));
         }
     }
 }
